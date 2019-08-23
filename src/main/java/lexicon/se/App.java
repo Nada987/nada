1e78fea0c30e56f6b3c73393ad88d1df502e6a19
Nada987 <nada.shehadeh@yahoo.com>
2019-08-23 15:51:02 +0200

    hiiiii

--- src/main/java/lexicon/se/App.java
+++ src/main/java/lexicon/se/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         Pants chinos = new Pants();
         chinos.length = 33;
-        chinos.width = 40;
+        chinos.width = 36;
 
         System.out.println("Woman pants with size " + chinos.getSize());
     }
